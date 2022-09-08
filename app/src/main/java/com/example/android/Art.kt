4fa70451044d9ee06f3c4c46db2593df9c1ec2dd
ibrahim <aydinibrahim1692@gmail.com>
2022-09-08 17:48:04 +0300

    Adapter class added

---
+++ app/src/main/java/com/example/android/Art.kt
@@ -0,0 +1,4 @@
+package com.example.android
+
+class Art(val name:String,val id:Int){
+}
--- app/src/main/java/com/example/android/FeedActivity.kt
+++ app/src/main/java/com/example/android/FeedActivity.kt
@@ -7,10 +7,31 @@ import android.view.Menu
 import android.view.MenuItem
 
 class FeedActivity : AppCompatActivity() {
+    private lateinit var artList:ArrayList<Art>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
+
+        artList=ArrayList<Art>()
+        try{
+            val database=this.openOrCreateDatabase("Arts", MODE_PRIVATE,null)
+            val cursor=database.rawQuery("SELECT * FROM arts",null)
+            val artNameIx=cursor.getColumnIndex("artname")
+            val idIx=cursor.getColumnIndex("id")
+            while(cursor.moveToNext()){
+                val name=cursor.getString(artNameIx)
+                val id=cursor.getInt(idIx)
+                val art=Art(name,id)
+                artList.add(art)
+            }
+            cursor.close()
+        }catch (e:Exception){
+            e.printStackTrace()
+        }
     }
+
+
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val menuInflater=menuInflater
         menuInflater.inflate(R.menu.art_menu,menu)
